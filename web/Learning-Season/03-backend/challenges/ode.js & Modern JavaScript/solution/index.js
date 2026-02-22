1c8e3a0d5d23dbe506b5089e438490afb16b0734
Abdenour Dio <abdnour2005@gmail.com>
2026-02-22 15:35:02 +0100

    feat: add input for city name with simple validation(no empty or number)

--- web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
+++ web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
-//CONSTANTS
-const API_KEY = process.env.API_KEY
-let city_name = "london"
+const prompt = require('prompt-sync')({ sigint: true }); // package for getting input
+
+const API_KEY = process.env.API_KEY // api_key
+let city_name;
+do {
+  city_name = prompt("enter a city name to get its weather status: ");
+
+  if (!city_name) {
+    console.log("city name cannot be empty. Try again");
+  } else if (/\d/.test(city_name)) // wch had syntax asahbi, hbit nchecki berk bli not a number
+  {
+    console.log("city name cannot contain numbers. Try again");
+
+  }
+}while (!city_name || /\d/.test(city_name)); // while empty or includes number
 async function getWeather() {
     const geocoding_response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_KEY}`)
     if (!geocoding_response.ok) {
@@ -9,7 +21,7 @@ async function getWeather() {
     }
     const geocoding_data = await geocoding_response.json() 
     city_name = geocoding_data['0']['name'] //updated city_name variable
-    const country = geocoding_data['0']['state']
+    const country = geocoding_data['0']['country']
     let lat = geocoding_data['0']['lat']
     let lon = geocoding_data['0']['lon']
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
