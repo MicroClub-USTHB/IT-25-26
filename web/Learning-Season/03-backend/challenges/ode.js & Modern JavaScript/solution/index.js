8d9a9381ef33166843c15708f77e1dbe30fdd3b5
Abdenour Dio <abdnour2005@gmail.com>
2026-02-22 15:12:34 +0100

    feat: add geocoding api to fetch lat and lon based on city_name

--- web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
+++ web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
@@ -1,19 +1,28 @@
 require('dotenv').config();
 //CONSTANTS
 const API_KEY = process.env.API_KEY
-const LAT = -76.67954
-const LON = -42.80039
-
+let city_name = "london"
 async function getWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}`)
+    const geocoding_response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_KEY}`)
+    if (!geocoding_response.ok) {
+        throw new Error("a problem has occured")
+    }
+    const geocoding_data = await geocoding_response.json() 
+    city_name = geocoding_data['0']['name'] //updated city_name variable
+    const country = geocoding_data['0']['state']
+    let lat = geocoding_data['0']['lat']
+    let lon = geocoding_data['0']['lon']
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
     if (!response.ok){
         throw new Error("a problem has occured")
     }
     const data = await response.json()
-    let weather = data['weather'][0]['main']
-    let temp = data['main']['temp']
-    let humidity = data['main']['humidity']
+    const weather = data['weather'][0]['main']
+    const temp = data['main']['temp']
+    const humidity = data['main']['humidity']
     const weather_status = {
+    "Country" : country,
+    "city" : city_name,
     "weather" : weather,
     "temp" : temp,
     "humidity" : humidity
