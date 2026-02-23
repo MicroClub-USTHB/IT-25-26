d69f4b585269c11550f5d30d9565ddefeb077516
Abdenour Dio <abdnour2005@gmail.com>
2026-02-23 21:06:43 +0100

    refactor: remove geocoding-api and use built-in API request by city name

--- web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
+++ web/Learning-Season/03-backend/challenges/ode.js & Modern JavaScript/solution/index.js
@@ -19,49 +19,37 @@ async function getWeather() {
         if (!API_KEY) {
             throw new Error("API-KEY is missing !!! check env file!!!!!!!")
         }
-        const geocoding_response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_KEY}`)
-        
-        if (geocoding_response.status === 401 || geocoding_response.status === 403){
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=${"metric"}&appid=${API_KEY}`)
+        if (response.status === 401 || response.status === 403){
             throw new Error("INVALID API KEY!!")
-        }        
-        if (geocoding_response.status === 504) {
-            throw new Error("Server error, try again later")
         }
-        if (geocoding_response.status === 408) {
-            throw new Error("slow or unstable client internet connection, check your internet")
-        }
-        const geocoding_data = await geocoding_response.json() 
-        if (geocoding_data.length === 0) // if city name was wrong
-            {
-            throw new Error("Invalid city name")
-        }
-        if (!geocoding_response.ok) {
-            throw new Error(`Unexpected error: ${geocoding_response.status}`)
-        }
-        city_name = geocoding_data['0']['name'] //updated city_name variable
-        const country = geocoding_data['0']['country']
-        let lat = geocoding_data['0']['lat']
-        let lon = geocoding_data['0']['lon']
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
         if (response.status === 504) {
             throw new Error("Server error, try again later")
         }
         if (response.status === 408) {
             throw new Error("slow or unstable client internet connection, check your internet")
         }
+        
+        const data = await response.json()
+        console.log(data)
+        if (response.status === 404) // if city name was wrong
+            {
+            throw new Error("Invalid city name")
+        }
         if (!response.ok) {
             throw new Error(`Unexpected error: ${response.status}`)
         }
-        const data = await response.json()
+        const country = data['sys']['country']
+        city_name = data['name'] //updated city_name variable
         const weather = data['weather'][0]['main']
         const temp = data['main']['temp']
         const humidity = data['main']['humidity']
         const weather_status = {
-        "Country" : country,
+        "country" : country,
         "city" : city_name,
         "weather" : weather,
-        "temp" : temp,
-        "humidity" : humidity
+        "temp" : `${temp}°C`,
+        "humidity" : `${humidity}%`
         }
         console.log(weather_status)
     }catch (error){
@@ -70,7 +58,8 @@ async function getWeather() {
     }
 }
 
-getWeather()
+
+getWeather() // dédicace l Anouar
 
 
 
